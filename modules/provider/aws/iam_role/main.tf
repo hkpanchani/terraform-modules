cc56c447031d5b979aa79cf6a30b19bcda1aa3e7
Hiren Panchani <git@hkpanchani.dev>
2021-11-11 14:08:05 +0530

    IAM_role module updated

--- modules/provider/aws/iam_role/main.tf
+++ modules/provider/aws/iam_role/main.tf
@@ -1,6 +1,12 @@
 resource "aws_iam_role" "main" {
   name = "${var.name}"
 
+  lifecycle {
+    ignore_changes = [
+      assume_role_policy,
+    ]
+  }
   assume_role_policy = "${var.assume_role_policy}"
+  managed_policy_arns = "${var.managed_policy_arns}"
 }
 
--- modules/provider/aws/iam_role/vars.tf
+++ modules/provider/aws/iam_role/vars.tf
@@ -3,3 +3,8 @@ variable "name" {}
 variable "assume_role_policy" {
     default = ""
 }
+
+variable "managed_policy_arns" {
+  default = []
+  type = list
+}
