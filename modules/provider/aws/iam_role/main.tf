767b71a99af43c8c50769b45b6ad654058acd7fe
Hiren Panchani <git@hkpanchani.dev>
2021-11-11 18:19:30 +0530

    iam_role updated to support jsonencode

--- modules/provider/aws/iam_role/main.tf
+++ modules/provider/aws/iam_role/main.tf
@@ -1,12 +1,7 @@
 resource "aws_iam_role" "main" {
   name = "${var.name}"
 
-  lifecycle {
-    ignore_changes = [
-      assume_role_policy,
-    ]
-  }
-  assume_role_policy = "${var.assume_role_policy}"
+  assume_role_policy = jsonencode("${var.assume_role_policy}")
   managed_policy_arns = "${var.managed_policy_arns}"
 }
 
