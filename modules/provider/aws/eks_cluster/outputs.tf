5aa2e841b15b897df2fc74de3ad76754bb59817f
Hiren Panchani <git@hkpanchani.dev>
2021-11-12 15:18:00 +0530

    eks_cluster_name exported

--- modules/provider/aws/eks_cluster/outputs.tf
+++ modules/provider/aws/eks_cluster/outputs.tf
@@ -1,7 +1,11 @@
 output "endpoint" {
-  value = aws_eks_cluster.main.endpoint
+  value = "${aws_eks_cluster.main.endpoint}"
+}
+
+output "eks_cluster_name" {
+  value = "${aws_eks_cluster.main.id}"
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.main.certificate_authority[0].data
+  value = "${aws_eks_cluster.main.certificate_authority[0].data}"
 }
