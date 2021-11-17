57217bf3448c489a7a8eca2c6f78378f556e04ff
Aman Makwana <aman.m@yudiz.com>
2021-11-17 09:05:17 +0000

    Update modules/provider/aws/ecr_repository/main.tf, modules/provider/aws/ecr_repository/outputs.tf, modules/provider/aws/ecr_repository/vars.tf

---
+++ modules/provider/aws/ecr_repository/vars.tf
@@ -0,0 +1,13 @@
+variable "resource_name" {
+  description = "Name of our ECR registery"
+  # default     = ""
+}
+variable "image_tag_mutability" {
+  description = "image tag mutability of docker images"
+  # default     = ""
+}
+variable "scan_on_push" {
+  description = "image vulnerability scanning"
+  # default     = ""
+  type = bool
+}
