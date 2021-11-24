f511d41a702202a2c1600f3c98792da383f1ea82
Yudiz Solutions <yudiz@Yudizs-Mac-mini.local>
2021-11-24 12:21:04 +0530

    submodule path updated

--- provider/aws/iam_access_key/main.tf
+++ provider/aws/iam_access_key/main.tf
@@ -1,6 +1,6 @@
 module "iam_user" {
   count = "${var.new_user}" ? 1:0 
-  source = "git::https://gitlab+deploy-token-639923:uxxxaC82yQEZqQYqE9kY@gitlab.com/yudiz_devops/terraform-modules.git//modules/provider/aws/iam_user"
+  source = "git::https://gitlab+deploy-token-639923:uxxxaC82yQEZqQYqE9kY@gitlab.com/yudiz_devops/terraform-modules.git//provider/aws/iam_user"
   name = "${var.user}"
   tags = {
       Created_by: "IAM access key"
