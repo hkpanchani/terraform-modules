331edbb633a60a784c4d95c7e9c7beed7c0d81d0
Hiren Panchani <git@hkpanchani.dev>
2021-12-07 16:59:28 +0530

    hotfix

--- provider/aws/route/v1/outputs.tf
+++ provider/aws/route/v1/outputs.tf
@@ -1,7 +1,7 @@
 output "is_gateway_route_id" {
-    value = "${var.is_gateway_id}"?"${aws_route.main_gateway_id[0].id}":""
+    value = "${var.is_gateway}"?"${aws_route.main_gateway_id[0].id}":""
 }
 
 output "is_nat_gateway_route_id" {
-    value = "${var.is_nat_gateway_id}"?"${aws_route.main_nat_gateway_id[0].id}":""
+    value = "${var.is_nat_gateway}"?"${aws_route.main_nat_gateway_id[0].id}":""
 }
